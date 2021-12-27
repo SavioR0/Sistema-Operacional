498450176c1dd6294f334198bd7132b941c9ee02
Gustavo Barcelos <gustavo05rb@gmail.com>
2021-12-27 10:23:22 -0300

    Correções finais

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,6 @@
 #include "../include/shell.hpp"
 
 
-
-
-
-
 int main(){
     system("clear");
     Kernel *kernel = new Kernel;
@@ -17,9 +13,8 @@ int main(){
 
     (*kernel).initialize();
 
-    while( (*shell).getOption()!= 6){
-        (*shell).selectedShellOption();
-        //setbuf(stdin, NULL); 
+    while( (*shell).getOption()!= 7){
+        (*shell).selectedShellOption(); 
 
         switch((*shell).getOption()){
             case 0: // help 
@@ -28,30 +23,33 @@ int main(){
             break;
             case 1: // meminfo 
                 
-                (*kernel).storage->print();
             
             break;
-            case 2: // cpuinfo 
+            case 2: //diskInfo
+                (*kernel).storage->print();
+
+            break;
+            case 3: // cpuinfo 
                 
                 (*kernel).cpu->print();
             
             break;
-            case 3: // queueschell 
+            case 4: // queueschell 
                 
                 cout<<"\n -Detalha quais processos estão sendo gerenciados pelo seu sistema, quais estão em estado de pronto, bloqueado, execução e/ou sendo criados e finalizados.\n";
             
             break;
-            case 4: // execute 
+            case 5: // execute 
                 
                 cout<<"\n -Executa a fila de processos definida conforme configuração prévia.\n";
             
             break;
-            case 5: // kill -9 
+            case 6: // kill -9 
                 
                 cout<<"\n -Finaliza a execucao do sistema operacional, voltando o mesmo para o estado inicial, em que os processos estariam ainda em fase de criação inicial.\n";
             
             break;
-            case 6: // exit 
+            case 7: // exit 
             
                 cout<<"\n -Saindo...\n";
             
@@ -67,58 +65,3 @@ int main(){
 
     return 0;
 }
-
-    /*Shell shell;
-    system("clear");
-    json processos;
-    ifstream("processo.json") >> processos;
-    //cout<< processos["processos"]["numProcessos"].dump(4)<<endl;
-
-    while(shell.getOption()!= 6){
-        shell.selectedShellOption();
-        //setbuf(stdin, NULL); 
-
-        switch(shell.getOption()){
-            case 0: // help 
-                shell.helpCommand();
-            
-            break;
-            case 1: // meminfo 
-                
-                cout<<"\n -Detalha quantas unidades de memória so sistema possui, quantas estão livres e quantas estão alocadas e para quais processos.\n";
-            
-            break;
-            case 2: // cpuinfo 
-                
-                cout<<"\n -Detalha qual processo está em execução, qual o timestamp e quantum desse.\n";
-            
-            break;
-            case 3: // queueschell 
-                
-                cout<<"\n -Detalha quais processos estão sendo gerenciados pelo seu sistema, quais estão em estado de pronto, bloqueado, execução e/ou sendo criados e finalizados.\n";
-            
-            break;
-            case 4: // execute 
-                
-                cout<<"\n -Executa a fila de processos definida conforme configuração prévia.\n";
-            
-            break;
-            case 5: // kill -9 
-                
-                cout<<"\n -Finaliza a execucao do sistema operacional, voltando o mesmo para o estado inicial, em que os processos estariam ainda em fase de criação inicial.\n";
-            
-            break;
-            case 6: // exit 
-            
-                cout<<"\n -Saindo...\n";
-            
-            break;
-            default:
-            
-                printf("\n -[ERRO 00] -> O comando informado nao existe.\nTente o comando 'help' para obter ajuda. \n");
-
-        }
-
-
-
-    };*/
--- src/shell.cpp
+++ src/shell.cpp
@@ -19,11 +19,12 @@ void Shell::selectedShellOption(){
 
     if     (str == "help"       ) option = 0;
     else if(str == "meminfo"    ) option = 1;
-    else if(str == "cpuinfo"    ) option = 2;
-    else if(str == "queueschell") option = 3;
-    else if(str == "execute"    ) option = 4;
-    else if(str == "kill -9"    ) option = 5;
-    else if(str == "exit"       ) option = 6;
+    else if(str == "diskinfo"   ) option = 2;
+    else if(str == "cpuinfo"    ) option = 3;
+    else if(str == "queueschell") option = 4;
+    else if(str == "execute"    ) option = 5;
+    else if(str == "kill -9"    ) option = 6;
+    else if(str == "exit"       ) option = 7;
     
     else option = -1; //caso errado 
 
@@ -32,6 +33,7 @@ void Shell::selectedShellOption(){
 void Shell::helpCommand(){
     cout<<"\n-> help\t\t -> Demonstra quais comandos podem ser executados pelo shell desenvolvido;\n"<<endl;
     cout<<"-> meminfo\t -> Detalha quantas unidades de memoria so sistema possui, quantas estão \n\t\t    livres e quantas estao alocadas e para quais processos.\n"<<endl;
+    cout<<"-> diskinfo\t -> Detalha qual e quantos blocos estão alocados em disco.\n"<<endl;
     cout<<"-> cpuinfo\t -> Detalha qual processo esta em execucao, qual o timestamp e quantum desse.\n"<<endl;
     cout<<"-> queueschell\t -> Detalha quais processos estão sendo gerenciados pelo seu sistema, \n\t\t    quais estão em estado de pronto, bloqueado, execucao e/ou sendo criados e finalizados.\n"<<endl;
     cout<<"-> execute\t -> Executa a fila de processos definida conforme configuracao previa. \n"<<endl;
