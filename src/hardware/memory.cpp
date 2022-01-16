e9ed0f5b1f45bea3f0d5292e7ea259358916e78f
SavioR0 <saviorodrigues012@gmail.com>
2022-01-16 19:47:04 -0300

    Implementaçao da inserçao e busca na memoria

--- src/hardware/memory.cpp
+++ src/hardware/memory.cpp
@@ -10,13 +10,39 @@ Memory::Memory(int segmentos){ //definindo o tamanho da tabela hash
 
 void Memory::clearMemory(int segmentos){
     for(int i=0; i<segmentos; i++){
-        ram[i].value = 0;
+        ram[i].value = -1;
         ram[i].description = "";
     }
 }
 
-//int Memory::insereMemory(MemoryContent mc){} 
-//int Memory::buscaMemory(int mat,MemoryContent mc){} 
+int Memory::hashingFunction(int key, int size){
+    return (key & 0x7FFFFFFF) % size;
+}
+
+int Memory::insertMemory(MemoryContent mc){
+    if(qtd == segments)
+        return 0;
+    cout<<""<<endl;
+    int key = mc.value;
+    int position = hashingFunction(key, segments);
+    struct MemoryContent* newmc;
+    newmc = (struct MemoryContent*)malloc(sizeof(struct MemoryContent));
+    if(newmc == NULL)
+        return 0;
+    *newmc = mc;
+    ram[position] = *newmc;
+    qtd++;
+    /* cout<<"\t-Inserçao na memoria concluída com sucesso!"<<endl; */
+    return 1;
+}
+
+int Memory::searchMemory(int id,MemoryContent* mc){
+    int pos = hashingFunction(id-1, segments);
+    if(ram[pos].value == -1)
+        return 0;
+    *mc = ram[pos]; 
+    return 1;
+} 
 
 void Memory::print(){
     /* cout<<"-Numero de Segmentos:"<<segments <<endl;
@@ -32,10 +58,10 @@ void Memory::print(){
     cout<<"  +  SEGMENTO\t|   DESCRIÇÃO\t|   CONTEÚDO MEMORIA\t|        ESTADO      \t+"<<endl;
     cout<<"---------------------------------------------------------------------------------"<<endl;
     for(int i = 0; i<segments; i++){
-        if(ram[i].value == 0 && ram[i].description=="")
-            cout<<"  +   ["<<i+1<<"]\t| "<<"     NULL"<<"\t|\t   "<<ram[i].value<<"\t\t|\t Livre  \t+"<<endl;
+        if(ram[i].value == -1 && ram[i].description=="")
+            cout<<"  +   ["<<i+1<<"]\t| "<<"     NULL"<<"\t|\t    -\t\t|\t Livre  \t+"<<endl;
         else
-            cout<<"  +   ["<<i+1<<"]\t|\t "<<ram[i].description<<"\t|\t"<<ram[i].value<<"\t|\tEm uso   \t+"<<endl;
+            cout<<"  +   ["<<i+1<<"]\t|  "<<ram[i].description<<"\t|\t    "<<ram[i].value<<"\t\t|\t  Em uso   \t+"<<endl;
     }
     cout<<"---------------------------------------------------------------------------------"<<endl;
 }
