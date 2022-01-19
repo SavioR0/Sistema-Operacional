142a7837cdcefc3dff47cc434b37ea5eb184e1d8
Gustavo Barcelos <gustavo05rb@gmail.com>
2022-01-19 20:44:46 -0300

    Escalonador funcional

--- include/hardware/storage.hpp
+++ include/hardware/storage.hpp
@@ -8,27 +8,31 @@ using namespace std;
 
 typedef struct BlockData BlockData;
 struct BlockData{
-    int key;
+    int    key;
     string type;
 
-    int time;
-    int currentTime;
-    bool   alocated = false;
+    int  time;
+    int  currentTime;
+    bool alocated = false;
 };
 
 class Storage{
     private:
+        int             size_blocks;      // Número de Blocos totais
+        int             allocated_blocks; // Blocos alocados
+        list<BlockData> blocks;           // Conteúdo e gerência de blocos  
+
     public:
-        int size;
-        int qtd;
-        list<BlockData> blocks;
 
         Storage(int size);
-        int insertStorage(BlockData* bd);
-        int removeStorage(int id);
+
         int check_time(int** ids);
         void addTimeStorage();
-        void insertBlockData(int key, string type, int randomNumber);
+
+        void insertBlockData(int key, string type, int random_number);
+        int  insertStorage(BlockData* block_data);
+        int  removeStorage(int id);
+        
         void print();
 };
 
